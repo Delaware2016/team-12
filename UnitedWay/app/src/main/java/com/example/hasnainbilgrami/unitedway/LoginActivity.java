5b788ad1e23a331c9ea8fd6c73dc245870134fdd
hasnainbilgrami <hrbilgra@gmail.com>
2016-10-07 20:22:26 -0400

    linked map

--- UnitedWay/app/src/main/java/com/example/hasnainbilgrami/unitedway/LoginActivity.java
+++ UnitedWay/app/src/main/java/com/example/hasnainbilgrami/unitedway/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.hasnainbilgrami.unitedway;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -186,6 +187,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent myIntent = new Intent(this, MapsActivity.class);
+
+            startActivity(myIntent);
         }
     }
     private boolean isEmailValid(String email) {
